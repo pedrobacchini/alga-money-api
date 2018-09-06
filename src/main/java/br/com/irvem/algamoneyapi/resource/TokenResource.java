2e8596984a30b9c6cfe3f3bb16ae3ae3108f48f9
Pedro Bacchini <pedrobacchini@outlook.com>
2018-09-06 14:44:10 -0300

    6.14. Implementando o logout

---
+++ src/main/java/br/com/irvem/algamoneyapi/resource/TokenResource.java
@@ -0,0 +1,26 @@
+package br.com.irvem.algamoneyapi.resource;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/token")
+public class TokenResource {
+
+    @DeleteMapping("/revoke")
+    public void revoke(HttpServletRequest request, HttpServletResponse response){
+        Cookie cookie = new Cookie("refreshToken", null);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false); //TODO: em producao sera true
+        cookie.setPath(request.getContextPath() + "/oauth/token");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        response.setStatus(HttpStatus.NO_CONTENT.value());
+    }
+}
