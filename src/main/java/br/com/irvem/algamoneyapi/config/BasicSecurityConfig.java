9c669b8cab5c54e804c4e77e16638ea9786f02d2
Pedro Bacchini <pedrobacchini@outlook.com>
2019-01-25 08:26:01 -0200

    Permitindo categorias

--- src/main/java/br/com/irvem/algamoneyapi/config/BasicSecurityConfig.java
+++ src/main/java/br/com/irvem/algamoneyapi/config/BasicSecurityConfig.java
@@ -32,6 +32,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/categorias").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic()
